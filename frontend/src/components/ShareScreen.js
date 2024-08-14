32124ce492d971b6d51d25c4a66a660f5c789922
odiedo <odiedopaul@gmail.com>
2024-08-14 09:23:27 +0300

    loading animation added

--- frontend/src/components/ShareScreen.js
+++ frontend/src/components/ShareScreen.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Button, Alert, Text } from 'react-native';
 import { getToken } from '../utils/auth';
+import LoadingAnimation from './LoadingAnimation';
 
-const ShareScreen = () => {
+const ShareScreen = ({navigation}) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const shareData = async () => {
+    setIsLoading(true);
     const token = await getToken();
     try {
       const response = await fetch('http://192.168.100.16:5000/api/share-data', {
@@ -17,16 +21,20 @@ const ShareScreen = () => {
       console.log(response); 
 
       const data = await response.json();
-
+      setIsLoading(false);
       if (response.ok) {
         Alert.alert('Success', data.message);
       } else {
         Alert.alert('Error', data.error || 'Something went wrong');
       }
     } catch (error) {
+      setIsLoading(false);
       Alert.alert('Error', error.message);
     }
   };
+  if (isLoading) {
+    return <LoadingAnimation />;
+  }
 
   return (
     <View style={{ margin: 20 }}>
